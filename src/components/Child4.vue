c4b8601371db46cbe9ca58d6010ca5fc0ab3eb4a
Richard Preston <ricardo@richards-air.home>
2021-12-04 20:27:48 +0100

    branch one first commit

---
+++ src/components/Child4.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="main">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Child4',
+}
+</script>
+
+<style scoped>
+  #main {
+    width: 300px;
+    height: 300px;
+    border: 1px solid purple;
+    margin-top: 20px;
+  }
+</style>
